5beece369d4484dcfa9db090dbb93ea2b1e5d93f
dominiccodespoti <dominic.codespoti@myob.com>
2019-08-05 14:49:30 +1000

    Working copy

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,11 +3,30 @@ public class StringCalculator {
     public int add(String s) {
         if (s.equals(""))
             return 0;
-        String[] split = s.split(",");
+
+        String delimiter = "";
+        String[] split = s.split(",|\n");
+        String negativeNumbers = "";
+
+        if (s.contains("//")) {
+            delimiter = String.valueOf(s.charAt(2));
+            split = s.split(",|\n|" + delimiter);
+        }
+
+        for (String e : split
+        ) {
+            if (Integer.parseInt(e) < 0)
+                negativeNumbers += e + ", ";
+        }
+
         int total = 0;
 
-        for (String e : split) {
-            total += Integer.parseInt(e);
+        for (int i = 0; i < split.length; i++) {
+            try {
+                total += Integer.parseInt(split[i]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
         }
         return total;
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,45 +2,80 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-    @Test public void emptyStringReturnsZero() {
+    @Test
+    public void emptyStringReturnsZero() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("");
         Assert.assertEquals(0, result);
     }
 
-    @Test public void oneReturnsOne() {
+    @Test
+    public void oneReturnsOne() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1");
         Assert.assertEquals(1, result);
     }
 
-    @Test public void threeReturnsThree() {
+    @Test
+    public void threeReturnsThree() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("3");
         Assert.assertEquals(3, result);
     }
 
-    @Test public void oneAndTwoReturnsThree() {
+    @Test
+    public void oneAndTwoReturnsThree() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1,2");
         Assert.assertEquals(3, result);
     }
 
-    @Test public void threeAndFiveReturnEight() {
+    @Test
+    public void threeAndFiveReturnEight() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("3,5");
         Assert.assertEquals(8, result);
     }
 
-    @Test public void oneAndTwoAndThreeReturnsSix() {
+    @Test
+    public void oneAndTwoAndThreeReturnsSix() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("1,2,3");
         Assert.assertEquals(6, result);
     }
 
-    @Test public void threeAndFiveAndThreeAndNineReturnTwenty() {
+    @Test
+    public void threeAndFiveAndThreeAndNineReturnTwenty() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("3,5,3,9");
         Assert.assertEquals(20, result);
+    }
+
+    @Test
+    public void oneAndTwoAndNewlineAndThreeReturnSix() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,2\n3");
+        Assert.assertEquals(6, result);
+    }
+
+    @Test
+    public void threeAndNewlineAndFiveAndNewlineAndThreeAndNineReturnTwenty() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("3\n5\n3,9");
+        Assert.assertEquals(20, result);
+    }
+
+    @Test
+    public void oneAndTwoWithDelimitersReturnThree() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("//;\n1;2");
+        Assert.assertEquals(3, result);
+    }
+
+    @Test
+    public void oneAndTwoAndThreeWithNegatives() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("//;\n1;2");
+        Assert.asse;
     }
 }
