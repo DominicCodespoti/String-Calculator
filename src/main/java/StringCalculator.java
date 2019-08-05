bc3327af7fa76d981b71d39aa4ef8a84b8a7ee33
dominiccodespoti <dominic.codespoti@myob.com>
2019-08-05 11:29:33 +1000

    Refactor step four

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,21 +1,14 @@
 public class StringCalculator {
 
     public int add(String s) {
-        if (s.contains(",")) {
-            String[] split = s.split(",");
-            int total = 0;
-
-            for (String e: split)
-            {
-                total += Integer.parseInt(e);
-            }
-            return total;
-        }
-
-        try {
-            return Integer.parseInt(s);
-        } catch (NumberFormatException e) {
+        if (s.equals(""))
             return 0;
+        String[] split = s.split(",");
+        int total = 0;
+
+        for (String e : split) {
+            total += Integer.parseInt(e);
         }
+        return total;
     }
 }
